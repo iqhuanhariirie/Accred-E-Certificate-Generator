80932c2311ad280af05fade968fce031c1dbb4a8
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-02-12 23:00:15 +0800

    fix: adjusted paddings

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -5,22 +5,22 @@ import AdminGuide from "@/components/AdminGuide.mdx";
 import Image from "next/image";
 
 const Page = () => {
-  return (
-    <>
-      <article className="prose dark:prose-invert py-10 m-auto">
-        <div className="flex flex-col justify-center items-center">
-          <Image
-            src={accredLs}
-            width={500}
-            alt="Accred Dark Logo"
-            className="mx-auto dark:invert"
-            priority
-          />
-        </div>
-        <AdminGuide />
-      </article>
-    </>
-  );
+	return (
+		<>
+			<article className="px-10 prose dark:prose-invert py-10 m-auto">
+				<div className="px-10 flex flex-col justify-center items-center">
+					<Image
+						src={accredLs}
+						width={500}
+						alt="Accred Dark Logo"
+						className="mx-auto dark:invert"
+						priority
+					/>
+				</div>
+				<AdminGuide />
+			</article>
+		</>
+	);
 };
 
 export default Page;
